f973b01b2cc49a5ec59881a0043f7243e74a1ec0
LittleVoid <116908347+LittleVoid@users.noreply.github.com>
2022-10-29 15:17:13 +0200

    Highscore Script angepasst
    funktioniert jetzt

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collectible : MonoBehaviour
 {
     public int ScoreAmount = 1;
-    
+    public Score ScoreManager;
+
+    private void Awake()
+    {
+        ScoreManager = GameObject.Find("Score").GetComponent<Score>();
+        Debug.Log(ScoreManager);
+    }
+
     void OnTriggerEnter(Collider other)
     {
-      if(other.name=="Player")
+        Debug.Log(other);
+        if(other.name=="Player")
         {
-            other.GetComponent<Score>().score++;
+            ScoreManager.AddScore(ScoreAmount);
             Destroy(gameObject);
         }
       
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public int score = 0;
+    [SerializeField] TextMeshProUGUI text;
 
-
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int ScoretoAdd)
     {
-        
+        score += ScoretoAdd;
+        text.text = score.ToString();
     }
 }
