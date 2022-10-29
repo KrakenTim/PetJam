3397d84bb9b535d88cdd7e1ac4bfa107dc9692d2
Florian Wiedenbein <45363205+DerSpeX@users.noreply.github.com>
2022-10-29 18:20:20 +0200

    Add Fishcontroller
    Added Fishcontroller

---
+++ Assets/Scripts/FishController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+    [SerializeField] float swimmingSpeed = 50f;
+    [SerializeField] float turnSpeed = 60f; 
+
+    Transform fishTransform;
+
+    private void Awake()
+    {
+        fishTransform = transform;
+    }
+
+    void Update()
+    {
+        Thrust();
+        Turn();
+    }
+
+    void Turn()
+    {
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+        fishTransform.Rotate(-pitch,yaw,-roll);
+    }
+
+    void Thrust()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            fishTransform.position += fishTransform.forward * swimmingSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        }
+    }
+}
