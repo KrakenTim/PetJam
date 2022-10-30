1a695c6b0e0ecdc030b8b0fc37ab0cb8d2d0817a
Pommelz <s.gsaenger@gmx.de>
2022-10-30 16:51:16 +0100

    cleaned scene and debug logs, added respawn mechanic for collectables

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -8,24 +8,42 @@ public class Collectible : MonoBehaviour
     public int ScoreAmount = 1;
     public Score ScoreManager;
     public AudioSource audioSource;
+    [SerializeField] private float maximumSpawnDuration;
+    private SphereCollider collider;
+    private MeshRenderer model;
 
     private void Awake()
     {
         ScoreManager = GameObject.Find("Score").GetComponent<Score>();
         audioSource = GetComponent<AudioSource>();
+        collider = GetComponent<SphereCollider>();
+        model = GetComponent<MeshRenderer>();
+    }
 
+    void ToggleVisibility()
+    {
+        collider.enabled = !collider.enabled;
+        model.enabled = !model.enabled;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.name=="Player")
+        if(other.tag == "Player")
         {
             ScoreManager.AddScore(ScoreAmount);
             audioSource.Play();
             //TODO: Add VFX here
-            Destroy(gameObject);
+            StartCoroutine("SpawnAfterLifetime");
         }
       
         
+    }
+
+    public IEnumerator SpawnAfterLifetime()
+    {
+        ToggleVisibility();
+        yield return new WaitForSeconds(maximumSpawnDuration);
+        ToggleVisibility();
+        yield return null;
     }
 }
