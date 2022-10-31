0c4642bb1dc8cf3dc04f624e25c9ab91543013da
Florian Wiedenbein <florian.zockt@gmail.com>
2022-10-31 13:38:31 +0100

    Bug Fixes & Settings Menu
    Added Setting Menus
    Fixed some minor Bugs

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    private Resolution[] _resolutions;
+   
+    
+    //GENERAL
+    void Start()
+    {
+        Cursor.visible = true;
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
+    //MAIN MENU
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void SettingsGame()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void CreditsGame()
+    {
+        SceneManager.LoadScene("CreditsMenu");
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    //SETTINGS MENU
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
